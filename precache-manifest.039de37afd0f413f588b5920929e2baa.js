90500b61862e4b08b505be81d8d6d7316f2d6ed8
Maria kozakov <cowmaria@gmail.com>
2019-04-27 00:56:40 +0300

    Updates

---
+++ precache-manifest.039de37afd0f413f588b5920929e2baa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/maria-minesweeper/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "7565abcbcd578f182594",
+    "url": "/maria-minesweeper/static/js/runtime~main.97629196.js"
+  },
+  {
+    "revision": "6c05f314ba655b0dcf8f",
+    "url": "/maria-minesweeper/static/js/main.9e97add1.chunk.js"
+  },
+  {
+    "revision": "50e616698c8a8f22b27a",
+    "url": "/maria-minesweeper/static/js/2.cce14b39.chunk.js"
+  },
+  {
+    "revision": "6c05f314ba655b0dcf8f",
+    "url": "/maria-minesweeper/static/css/main.ab747846.chunk.css"
+  },
+  {
+    "revision": "a559680e0daccaa63d701fa88f7fc367",
+    "url": "/maria-minesweeper/index.html"
+  }
+];

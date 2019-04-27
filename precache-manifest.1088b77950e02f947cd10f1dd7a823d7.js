dfee8b530feddb3a807d1a5f1a42cae3fc367779
Maria kozakov <cowmaria@gmail.com>
2019-04-28 02:28:31 +0300

    Updates

---
+++ precache-manifest.1088b77950e02f947cd10f1dd7a823d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7565abcbcd578f182594",
+    "url": "/maria-minesweeper/static/js/runtime~main.97629196.js"
+  },
+  {
+    "revision": "268ac19115cc72aff595",
+    "url": "/maria-minesweeper/static/js/main.9f78308e.chunk.js"
+  },
+  {
+    "revision": "e269fc96842a971477b7",
+    "url": "/maria-minesweeper/static/js/2.cf04b197.chunk.js"
+  },
+  {
+    "revision": "268ac19115cc72aff595",
+    "url": "/maria-minesweeper/static/css/main.632c21f2.chunk.css"
+  },
+  {
+    "revision": "a53e3a039124557d3f8e54d68870f06d",
+    "url": "/maria-minesweeper/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/maria-minesweeper/precache-manifest.8023695861e5ddd1cb2a96396a7a2eab.js"
+  "/maria-minesweeper/precache-manifest.1088b77950e02f947cd10f1dd7a823d7.js"
 );
 
 workbox.clientsClaim();
